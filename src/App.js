34aac619d406609cb772a1431ae9e6e3b230eaf2
ibaranau1 <ibaranau1@exadel.com>
2023-11-27 20:47:49 +0100

    feat: added sort by for items

--- src/App.js
+++ src/App.js
@@ -86,11 +86,26 @@ function Form({ onAddItems }) {
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
   console.log(items);
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "discription")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
 
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -99,6 +114,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="discription">Sort by input discription</option>
+          <option value="packed">Sort by input packed</option>
+        </select>
+      </div>
     </div>
   );
 }
